80791fe801b75156d612db6904957da8b1310e09
Islam Shekerbekov <megazyamba@gmail.com>
2023-04-20 19:32:44 +0600

    add adaptive for header-top

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
-$(function(){
+const MENU_BTN = document.querySelector('#menu__btn');
+const MENU_LIST = document.querySelector('#menu__list')
 
+MENU_BTN.addEventListener('click', () => {
+  MENU_BTN.classList.toggle('active');
+  MENU_LIST.classList.toggle('active');
 });
