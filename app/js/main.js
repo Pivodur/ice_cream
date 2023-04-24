fafdf07212d202a288e5085a3d1a9dda156bd87f
Islam Shekerbekov <megazyamba@gmail.com>
2023-04-24 22:02:37 +0600

    add review section and adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,20 @@
-const MENU_BTN = document.querySelector('#menu__btn');
-const MENU_LIST = document.querySelector('#menu__list')
+const mySwiper = new Swiper('.slider', {
+  slidesPerView: 1,
+  spaceBetween: 30,
 
-MENU_BTN.addEventListener('click', () => {
-  MENU_BTN.classList.toggle('active');
-  MENU_LIST.classList.toggle('active');
+  pagination: {
+    el: '.slider__pagination',
+    clickable: true,
+  }
+});
+
+
+
+
+const MENU_BTN = document.querySelector("#menu__btn");
+const MENU_LIST = document.querySelector("#menu__list");
+
+MENU_BTN.addEventListener("click", () => {
+  MENU_BTN.classList.toggle("active");
+  MENU_LIST.classList.toggle("active");
 });
